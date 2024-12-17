343d541ab546a78ae156760d639920528f65f082
jangrahiten <jangrahiten.dev@gmail.com>
2024-12-18 00:37:55 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='font-bold text-red-700 flex justify-center'>
+      Hello World
+    </div>
+  )
+}
+
+export default page
